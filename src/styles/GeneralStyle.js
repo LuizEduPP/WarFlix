6821533921212b59bf148f4e2421c40f7c03921b
Luiz Eduardo <2811luiz@gmail.com>
2019-08-12 20:46:16 -0300

    remove unused style

--- src/styles/GeneralStyle.js
+++ src/styles/GeneralStyle.js
@@ -1,20 +1,6 @@
 import { StyleSheet } from "react-native";
 
 const GeneralStyle = StyleSheet.create({
-//   card: {
-//     //paddingHorizontal: 0,
-//     //paddingTop: 0,
-//     //margin: -1 -2 -0 -2
-//   },
-//   Header: {
-//     backgroundColor: "#29B6F6"
-//   },
-//   StatusBar: {
-//     backgroundColor: "#C23616"
-//   },
-//   Container: {
-//     backgroundColor: "#FFF"
-//   },
     touchableOpacity: {
 		paddingTop: 20,
 		paddingBottom: 20,
